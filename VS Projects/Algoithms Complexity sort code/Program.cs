218511bfb9e7f740e061287735da605e5fe4cda4
DeaconKaiJ <61854430+DeaconKaiJ@users.noreply.github.com>
2020-03-06 00:32:59 +0000

    Add files via upload

---
+++ VS Projects/Algoithms Complexity sort code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+
+namespace Algoithms_Complexity_sort_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read text file content
+
+            string text = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "Net_1_256\\Net_1_256.txt"));
+            Console.WriteLine(text);
+        }
+    }
+}
